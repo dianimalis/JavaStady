274483d7bc318eb516752c48a82097e73da14669
dianimalis <4426@ukr.net>
2018-02-07 04:29:29 +0200

    proces

--- src/Modul_5/HomeWork_5/Main.java
+++ src/Modul_5/HomeWork_5/Main.java
@@ -1,7 +1,25 @@
 package Modul_5.HomeWork_5;
 
+import Modul_5.HomeWork_5.CarDoor.CarDoor;
+
 /**
  * Created by dianimalis on 06.02.2018.
  */
 public class Main {
+    public static void main(String[] args) {
+
+CarDoor doorClose = new CarDoor();
+CarDoor doorOpen = new CarDoor();
+CarDoor windowOpen = new CarDoor();
+CarDoor windowClose = new CarDoor();
+
+
+
+
+        System.out.println("Закрыть " + doorClose.doorClose + " дверь");
+        System.out.println("Открыть " + doorOpen.doorOpen + " дверь");
+        System.out.println("Закрыть " + windowClose.windowClose + " окно");
+        System.out.println("Открыть " + windowOpen.windowOpen + " окно");
+
+    }
 }
