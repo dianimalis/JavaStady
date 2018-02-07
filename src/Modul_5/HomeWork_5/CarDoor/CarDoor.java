15d66fb379168650842bb190d2367058c9353e86
dianimalis <4426@ukr.net>
2018-02-07 04:29:41 +0200

    po

--- src/Modul_5/HomeWork_5/CarDoor/CarDoor.java
+++ src/Modul_5/HomeWork_5/CarDoor/CarDoor.java
@@ -1,9 +1,38 @@
-package Modul_5.HomeWork_5;
+package Modul_5.HomeWork_5.CarDoor;
 
 /**
  * Created by dianimalis on 07.02.2018.
  */
 public class CarDoor {
-    private Door[] doors;
-    
+
+    public String doorClose;
+    public String doorOpen;
+    public String windowClose;
+    public String windowOpen;
+
+
+
+    //public CarDoor() { // Конструктор класса, вызввается тогда, когда мы создаем объект этого класса
+//        door = "Close door";
+//        window = "Close window";
+    //}
+
+
+//    public CarDoor(String door, String window){
+//        this.door = door;
+//        this.window = window;
+    //}
+//    public void show(){
+//        System.out.println("Door is: " + door);
+//        System.out.println("Window is: " + window);
+//
+//    }
+    public CarDoor(){
+       this.doorClose = ("Close");
+        this.doorOpen = ("Open");
+        this.windowClose = ("Close");
+        this.windowOpen = ("Open");
+    }
+
+
 }
