776e337ca6ab6e0076863f049f1ea2d6cf200b5f
dianimalis <4426@ukr.net>
2018-02-11 02:32:30 +0200

    + Masha

--- src/Modul_5/FinalExample.java
+++ src/Modul_5/FinalExample.java
@@ -4,4 +4,16 @@ package Modul_5;
  * Created by dianimalis on 07.02.2018.
  */
 public class FinalExample {
+    final int releaseDate;
+
+    public FinalExample(){
+        this.releaseDate = 10;
+    }
+
+    static final float PI = 3.14f; // вынос за пределы метода, как поля класса
+
+    public static void main(String[] args) {
+        //final float PI = 3.14f; // если внутри метода
+        float pi = PI;
+    }
 }
